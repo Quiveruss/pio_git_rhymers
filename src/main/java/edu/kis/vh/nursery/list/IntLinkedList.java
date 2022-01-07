ff658295623d920d307d6b0582c620af1be32488
pk-9 <pk-9@sr.dmcs.pl>
2022-01-07 19:38:14 +0100

    1.4 stale 1 - Deklaracja stałych oraz podmiana powtarzalnych wartości

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,8 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+	private final int EMPTY_LINKED_LIST = -1;
+
 	Node last;
 	int i;
 
@@ -25,13 +27,13 @@ public class IntLinkedList {
 
 	public int top() {
 		if (isEmpty())
-			return -1;
+			return EMPTY_LINKED_LIST;
 		return last.value;
 	}
 
 	public int pop() {
 		if (isEmpty())
-			return -1;
+			return EMPTY_LINKED_LIST;
 		int ret = last.value;
 		last = last.prev;
 		return ret;
